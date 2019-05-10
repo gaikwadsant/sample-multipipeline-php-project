0541355d8eb608b974d3126418057c1e5c6a54aa
root <root@ip-172-31-22-205.us-east-2.compute.internal>
2019-05-10 04:59:13 +0000

    Add SemVer.groovy file

--- SemVer.groovy
+++ SemVer.groovy
@@ -1,4 +1,3 @@
-
 def getTagversion (String oldtagVersion)
 {
     oldtagVersion = oldtagVersion.
@@ -10,34 +9,34 @@ def getTagversion (String oldtagVersion)
     minor = versionParts[1].toInteger()
     timetag = versionParts[2].toString()
     this.month = timetag.substring(4,6).toInteger()
-
-
+    
+    
     Calendar now = Calendar.getInstance();
     currmonth = (now.get(Calendar.MONTH) +1)
-
+    
     Date date = new Date()
     //println date.getTime()
     //println date.format( 'yyyy-MM-ddHH:mm:ss.S' )
     String newtimetag = date.format( 'yyyyMMdd-HHmm' ).toString()
     //println strtimetag
     //timetag= timetag.replaceAll(":","-")
-
-
+    
+    
     if (currmonth==month)
     {
-
+        
         version =  major+ "." + minor + "." +newtimetag
     }
     else
     {
       if (month < 9)
       {
-
+    
         minor = minor +1
         version = major+ "." + minor + "." +newtimetag
-
+                
       }
-      else
+      else 
       {
         major = this.major +1
         minor = 0
